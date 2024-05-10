af6a57c3c3e8ea9db64db76033bad5e6d660635a
Pedro Monteiro <montteiropedro@gmail.com>
2024-05-10 15:52:46 -0300

    SandboxLive

---
+++ lib/live_study/sandbox.ex
@@ -0,0 +1,17 @@
+defmodule LiveStudy.Sandbox do
+  def calculate_weight(length, width, depth) do
+    (to_float(length) * to_float(width) * to_float(depth) * 7.3)
+    |> Float.round(1)
+  end
+
+  def calculate_price(weight), do: weight * 0.15
+
+  defp to_float(value) when is_binary(value) do
+    case Float.parse(value) do
+      {float, _} -> float
+      :error -> 0.0
+    end
+  end
+
+  defp to_float(value), do: value
+end
--- lib/live_study_web/router.ex
+++ lib/live_study_web/router.ex
@@ -20,6 +20,7 @@ defmodule LiveStudyWeb.Router do
     get "/", PageController, :home
 
     live "/light", LightLive
+    live "/sandbox", SandboxLive
   end
 
   # Other scopes may use custom stacks.
